dcbd7c9baf0c2c74d5e56614f12b0ae995ed4078
HolyCoder13 <adrian.madejski@vp.pl>
2023-12-11 20:17:18 +0100

    memory based h2-db connection aquired

---
+++ MedicalSystem/target/classes/schema.sql
@@ -0,0 +1,8 @@
+create table patient(
+    id int auto_increment,
+    name varchar2(100) not null ,
+    surname varchar2(100) not null ,
+    age int not null,
+    description varchar2(4096) not null
+
+);
